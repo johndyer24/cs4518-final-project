45dbc95a14ab759c114a43dc7afd127c6d98c9af
johndyer24 <johndyer24@gmail.com>
2017-12-10 16:34:43 -0500

    setup adapter and viewholder for recyclerview in ChatActivity

--- TheRoom/app/src/main/java/com/example/android/theroom/ChatActivity.java
+++ TheRoom/app/src/main/java/com/example/android/theroom/ChatActivity.java
@@ -5,11 +5,30 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.theroom.models.Message;
+import com.marshalchen.ultimaterecyclerview.UltimateRecyclerView;
+import com.marshalchen.ultimaterecyclerview.UltimateRecyclerviewViewHolder;
+import com.marshalchen.ultimaterecyclerview.UltimateViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
     private String mUserName;
     private String mChatID;
+
+    private UltimateRecyclerView mMessageRecyclerView;
+    private TextView mLoadingTextView;
+    private ChatAdapter mAdapter;
+    private List<Message> mMessagesList;
 
     /**
      * Static method that returns intent used to start MainActivity
@@ -38,5 +57,128 @@ public class ChatActivity extends AppCompatActivity {
             bar.setTitle(getString(R.string.chat_activity_title, mUserName));
         }
 
+        // setup Adapter and RecyclerView
+        mMessagesList = new ArrayList<>();
+        mAdapter = new ChatAdapter(mMessagesList);
+        mMessageRecyclerView = findViewById(R.id.message_recycler_view);
+        final LinearLayoutManager manager = new LinearLayoutManager(this);
+        mMessageRecyclerView.setLayoutManager(manager);
+        mMessageRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    /**
+     * The RecyclerView Viewholder
+     */
+    private class ChatHolder extends UltimateRecyclerviewViewHolder implements View.OnClickListener {
+
+        private TextView mTextView;
+        private Message mMessage;
+        private int mPosition;
+
+        public ChatHolder(View itemView) {
+            super(itemView);
+
+            mTextView = (TextView) itemView.findViewById(R.id.message_list_row_textview);
+            itemView.setOnClickListener(this);
+        }
+
+        /**
+         * Used by Chat Adapter to bind Message object to the View
+         * @param message
+         * @param position
+         */
+        public void bind(Message message, int position) {
+            mMessage = message;
+            String text = mMessage.getText();
+            mTextView.setText(text);
+            mPosition = position;
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    /**
+     * The RecyclerView Adapter
+     */
+    private class ChatAdapter extends UltimateViewAdapter<ChatActivity.ChatHolder> {
+
+        private List<Message> mMessages;
+
+        public ChatAdapter(List<Message> messages) {
+            mMessages = messages;
+        }
+
+        public void setMessages(List<Message> message) {
+            mMessages = message;
+        }
+
+        @Override
+        public ChatActivity.ChatHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.message_list_row, parent, false);
+            ChatActivity.ChatHolder vh = new ChatActivity.ChatHolder(v);
+            return vh;
+        }
+
+        @Override
+        public ChatActivity.ChatHolder newFooterHolder(View view) {
+            return null;
+        }
+
+        @Override
+        public ChatActivity.ChatHolder newHeaderHolder(View view) {
+            return null;
+        }
+
+        @Override
+        public ChatActivity.ChatHolder onCreateViewHolder(ViewGroup parent) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.message_list_row, parent, false);
+            ChatActivity.ChatHolder vh = new ChatActivity.ChatHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ChatActivity.ChatHolder holder, int position) {
+            holder.bind(mMessages.get(position), position);
+        }
+
+        @Override
+        public ChatActivity.ChatHolder onCreateHeaderViewHolder(ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            if (mMessages == null) {
+                return 0;
+            } else {
+                return mMessages.size();
+            }
+        }
+
+        @Override
+        public int getAdapterItemCount() {
+            if (mMessages == null) {
+                return 0;
+            } else {
+                return mMessages.size();
+            }
+        }
+
+        @Override
+        public long generateHeaderId(int position) {
+            return 0;
+        }
     }
 }
+
