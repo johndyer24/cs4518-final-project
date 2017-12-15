bb48b5e9dae04a18e74da5251b98b346b7c4bec8
johndyer24 <johndyer24@gmail.com>
2017-12-14 22:23:30 -0500

    bug fix

--- TheRoom/app/src/main/java/com/example/android/theroom/NewChatActivity.java
+++ TheRoom/app/src/main/java/com/example/android/theroom/NewChatActivity.java
@@ -251,6 +251,12 @@ public class NewChatActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Chat c = dataSnapshot.getValue(Chat.class);
                 c.setChatID(dataSnapshot.getKey());
+                Log.d(TAG, etChatID());
+                Log.d(TAG, c.getUser1());
+                Log.d(TAG, c.getUser2());
+                Log.d(TAG, c.getUser1DisplayName());
+                Log.d(TAG, c.getUser2DisplayName());
+
                 Intent i;
                 if (c.getUser1().equals(mAuth.getUid())) {
                     i = ChatActivity.newIntent(getApplicationContext(), c.getUser2(), c.getUser2DisplayName(), c.getChatID());
